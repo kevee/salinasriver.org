79ffeee0c39e9c9843d9d565acd3fb5471aa6ad2
Kevin Miller <keveemiller@gmail.com>
2021-08-24 07:59:56 -0700

    chore: Log guage

--- src/_tools/guages.js
+++ src/_tools/guages.js
@@ -26,8 +26,12 @@ const run = async () => {
   ).then((response) => response.json())
 
   const waterLevels = {}
+  let highest = 0
 
   results.value.timeSeries.forEach((item) => {
+    if (parseFloat(item.values[0].value[0].value, 10) > highest) {
+      highest = parseFloat(item.values[0].value[0].value, 10)
+    }
     waterLevels[guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].name] =
       {
         cfs: parseFloat(item.values[0].value[0].value, 10),
@@ -36,6 +40,9 @@ const run = async () => {
         label: guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].label,
       }
   })
+  console.log(
+    `Wrote ${guages.length} water guages. Highest guage is ${highest}`
+  )
   fs.writeJSONSync('./src/_data/guages.json', waterLevels)
 }
 
