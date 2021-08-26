08a742da296a5619357ee2ebe9bd77458e66906e
Kevin Miller <keveemiller@gmail.com>
2021-08-26 16:23:40 -0700

    chore: Remove images

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,5 @@
 const markdownIt = require('markdown-it')
 const { DateTime } = require('luxon')
-const Image = require('@11ty/eleventy-img')
-
-async function imageShortcode(src, alt, sizes) {
-  let metadata = await Image(src, {
-    widths: [300, 600],
-    formats: ['avif', 'jpeg'],
-    outputDir: './public/img',
-  })
-
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: 'lazy',
-    decoding: 'async',
-  }
-
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes)
-}
 
 const md = new markdownIt({
   html: true,
@@ -28,7 +9,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/sass/')
   eleventyConfig.addPassthroughCopy('./src/assets')
   eleventyConfig.addPassthroughCopy('./src/_original')
-  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
 
   eleventyConfig.addFilter('markdown', (content) => {
     return md.render(content)
