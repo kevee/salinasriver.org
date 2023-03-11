afc889ed52e290e0c28aa886954136e5a3e8a9e8
Kevin Miller <kevin@kevee.net>
2023-03-11 05:36:46 -0800

    feat: auto-generate flood warning

--- src/_tools/guages.js
+++ src/_tools/guages.js
@@ -5,17 +5,20 @@ const guages = {
   11152300: {
     name: 'chualar',
     label: 'Chualar',
+    flood: 9,
   },
   11150500: {
     name: 'bradley',
     label: 'Bradley Bridge',
+    flood: 12,
   },
   11151700: {
     name: 'soledad',
     label: 'Soledad',
+    flood: 13,
   },
-  11147500: { name: 'paso', label: 'Paso Robles' },
-  11152500: { name: 'spreckles', label: 'Spreckles' },
+  11147500: { name: 'paso', label: 'Paso Robles', flood: 18 },
+  11152500: { name: 'spreckles', label: 'Spreckles', flood: 15 },
 }
 
 const run = async () => {
@@ -28,6 +31,8 @@ const run = async () => {
 
   const waterLevels = {}
   let highest = 0
+
+  let isFlooding = false
 
   results.value.timeSeries.forEach((item) => {
     if (parseFloat(item.values[0].value[0].value, 10) > highest) {
@@ -59,12 +64,24 @@ const run = async () => {
         guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].name
       ].cfs = parseFloat(item.values[0].value[0].value, 10)
     }
+    if (
+      waterLevels[guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].name]
+        .height >= guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].flood
+    ) {
+      isFlooding = true
+    }
+    waterLevels[
+      guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].name
+    ].floodLevel = guages[parseInt(item.sourceInfo.siteCode[0].value, 10)].flood
   })
   console.log(
     `Wrote ${
       Object.keys(waterLevels).length
     } water guages. Highest guage is ${highest}`
   )
+  fs.writeJSONSync('./src/_data/warning.json', {
+    isFlooding,
+  })
   fs.writeJSONSync('./src/_data/guages.json', waterLevels)
 }
 
