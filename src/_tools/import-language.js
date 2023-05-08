4b2638fec1629d046c95fb8853e27c988d38daef
Kevin Miller <kmiller@usdigitalresponse.org>
2023-05-08 13:32:15 -0700

    better line breaks

--- src/_tools/import-language.js
+++ src/_tools/import-language.js
@@ -18,11 +18,11 @@ for (const line of translate.split('\n')) {
   if (current) {
     const index = strings.findIndex((s) => s.key === current)
     if (index !== -1) {
-      strings[index].value += line
+      strings[index].value += '\n' + line
     } else {
       strings.push({
         key: current,
-        value: line.trim(),
+        value: line,
       })
     }
   }
@@ -69,6 +69,129 @@ for (const string of strings) {
       front.__content = string.value
       fs.writeFileSync(
         `./src/es/access-points/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+  }
+
+  if (key[0] === 'overview') {
+    const file = fs
+      .readFileSync(`./src/es/overview/${key[1]}.md`, 'utf8')
+      .toString()
+    const front = yamlFront.loadFront(file)
+    if (key[2] === 'title') {
+      front.title = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/overview/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+    if (key[2] === 'content') {
+      front.__content = string.value
+      fs.writeFileSync(
+        `./src/es/overview/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+  }
+
+  if (key[0] === 'legal') {
+    const file = fs
+      .readFileSync(`./src/es/legal/${key[1]}.md`, 'utf8')
+      .toString()
+    const front = yamlFront.loadFront(file)
+    if (key[2] === 'title') {
+      front.title = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/legal/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+    if (key[2] === 'content') {
+      front.__content = string.value
+      fs.writeFileSync(
+        `./src/es/legal/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+  }
+
+  if (key[0] === 'trip') {
+    const file = fs
+      .readFileSync(`./src/es/trips/${key[1]}.md`, 'utf8')
+      .toString()
+    const front = yamlFront.loadFront(file)
+    if (key[2] === 'title') {
+      front.title = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+    if (key[2] === 'alert') {
+      front.alert = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+
+    if (key[2] === 'gear') {
+      front.gear = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+
+    if (key[2] === 'setUp') {
+      front.setUp = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+
+    if (key[2] === 'safety') {
+      front.safety = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+
+    if (key[2] === 'parts' && key[4] === 'title') {
+      front.parts[key[3]].title = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+
+    if (key[2] === 'parts' && key[4] === 'description') {
+      front.parts[key[3]].description = string.value.trim()
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
+        yamlToString(front),
+        'utf8'
+      )
+    }
+
+    if (key[2] === 'content') {
+      front.__content = string.value
+      fs.writeFileSync(
+        `./src/es/trips/${key[1]}.md`,
         yamlToString(front),
         'utf8'
       )
